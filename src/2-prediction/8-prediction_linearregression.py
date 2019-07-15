e35c0feaf5574cf9a39fc3a81b4306da45ce2e3f
Mehmet Emin Paca <meminpaca@gmail.com>
2019-07-15 23:55:47 +0300

    upload data

--- src/2-prediction/8-prediction_linearregression.py
+++ src/2-prediction/8-prediction_linearregression.py
@@ -28,16 +28,7 @@ sales = data[["Sales"]]
 #Split data as train and test
 from sklearn.model_selection import train_test_split
 x_train, x_test,y_train,y_test = train_test_split(months,sales,test_size=0.33, random_state=0)
-'''
-#Data scaling
-from sklearn.preprocessing import StandardScaler
 
-sc = StandardScaler()
-X_train = sc.fit_transform(x_train)
-X_test = sc.fit_transform(x_test)
-Y_train = sc.fit_transform(y_train)
-Y_test = sc.fit_transform(y_test)
-'''
 # build model (linear regression)
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
@@ -46,7 +37,7 @@ lr.fit(x_train,y_train)
 prediction = lr.predict(x_test)
 print(prediction)
 
-#Sort for sequential display on the chart
+#Sort for sequential display on the chart, sorts by index of record
 x_train = x_train.sort_index()
 y_train = y_train.sort_index()
 plt.plot(x_train,y_train)
@@ -57,17 +48,5 @@ plt.xlabel("Months")
 plt.ylabel("Sales")
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-    
     
 
