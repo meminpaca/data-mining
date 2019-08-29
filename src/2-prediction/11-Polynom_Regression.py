3366a9d0aea37f04f5a01d579239d479c8181e03
Mehmet Emin Paca <meminpaca@gmail.com>
2019-08-29 16:20:44 +0300

    upload data

--- src/2-prediction/11-Polynom_Regression.py
+++ src/2-prediction/11-Polynom_Regression.py
@@ -6,49 +6,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import Imputer
 
 # 2 - Data Preprocessing Steps
 
 # 2.1 - Load data set
 data = pd.read_csv('../../data/salaries.csv')
 
-# 2.2 Split data as test and train data
-from sklearn.model_selection import train_test_split
-#x_train, x_test, y_train, y_test = train_test_split(s,result3, test_size=0.33, random_state=0)
-
+# 2.2 Slice Data Frames
+# get education level column
 x = data.iloc[:,1:2]
+# get salary column
 y = data.iloc[:,2:]
 
+# convert dataframe to numpy array
 X = x.values
 Y = y.values
 
+# linear regression
 from sklearn.linear_model import LinearRegression
-
 lin_reg = LinearRegression()
 lin_reg.fit(X, Y)
-
-
-
-
-#plt.scatter(X,Y,color='red')
-#plt.plot(X, lin_reg.predict(X), color='blue')
-#plt.show()
-
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg  = PolynomialFeatures(degree=4)
-x_poly = poly_reg.fit_transform(X)
-print(x_poly)
-lin_reg2 = LinearRegression()
-lin_reg2.fit(x_poly,y)
 plt.scatter(X,Y,color='red')
-plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)),color="blue")
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.show()
+
+# polynomial regression(2nd degree)
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg2  = PolynomialFeatures(degree=2)
+x_poly2 = poly_reg2.fit_transform(X)
+#print(x_poly)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly2,y)
+plt.scatter(X,Y,color='red')
+plt.plot(X, lin_reg2.predict(poly_reg2.fit_transform(X)),color="blue")
+plt.show()
+
+
+# polynomial regression (4th degree)
+poly_reg4  = PolynomialFeatures(degree=4)
+x_poly4 = poly_reg4.fit_transform(X)
+#print(x_poly)
+lin_reg4 = LinearRegression()
+lin_reg4.fit(x_poly4,y)
+plt.scatter(X,Y,color='red')
+plt.plot(X, lin_reg4.predict(poly_reg4.fit_transform(X)),color="blue")
 plt.show()
 
 
 # sample predictions
-print(lin_reg2.predict(poly_reg.fit_transform(np.array([[6]]))))
-print(lin_reg2.predict(poly_reg.fit_transform(np.array([[11]]))))
+print(lin_reg2.predict(poly_reg2.fit_transform(np.array([[6]]))))
+print(lin_reg2.predict(poly_reg2.fit_transform(np.array([[11]]))))
 
+# sample predictions
+print(lin_reg4.predict(poly_reg4.fit_transform(np.array([[6]]))))
+print(lin_reg4.predict(poly_reg4.fit_transform(np.array([[11]]))))
 
 
